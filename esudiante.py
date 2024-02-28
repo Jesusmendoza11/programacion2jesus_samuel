2b330cb006c0307d5c5dea9a8600a0111eb30c7f
Jesusmendoza11 <160240333+Jesusmendoza11@users.noreply.github.com>
2024-02-28 11:18:15 -0500

    Add files via upload

---
+++ esudiante.py
@@ -0,0 +1,26 @@
+
+class estudiante:
+     def __int__(self,nombre,apellido,cedula,correo,telefono):
+
+      self.nombre=nombre
+     
+      self.apellido=apellido
+     
+      self.cedula=cedula
+     
+      self.correo=correo
+     
+      self.telefono=telefono
+
+def obtenernombre(self):
+        return f'mi nombre es {self.nombre}'
+def obtenerapellido(self):
+         return f' mi apellido es{self.apellido}'
+def obtenercedula(self):
+         return f'mi cedula es {self.cedula}'
+def obtenercorreo(self):
+         return f'mi correo es {self.correo}'
+def obtenertelefono(self):
+         return f'mi numero telefono es{self.telefono}'
+        
+print("nombre jesus","apellido mendoza","cedula 1043641605","correo jesusamuelmendoza@gmail.com","telefono 3183990057")
---
+++ main.py
@@ -0,0 +1,16 @@
+from estudiante import estudiante
+
+p=persona("jesus","apellido")
+print(P.Obtenernombre())
+
+p=persona("jesus","mendoza")
+print(p.Obtenerapellido())
+
+p=persona("jesus","mendoza","1043641605")
+print(p.Obtenercedula())
+
+p=persona("jesus","mendoza","1043641605","jesusamuelmendoza@gmail.com")
+print(p.obtenercorreo())
+
+p=persona("jesus","mendoza","jesusamuelmendoza@gmail.com","3183990057")
+print(p.Obtenertelefono())
